fefe2630f4d231b64a95c39abeba5c0b517731fa
Crowdbotics <team@crowdbotics.com>
2023-04-14 05:47:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-term-39922",
+  url: "https://withered-term-39922.botics.co",
+  api: "https://withered-term-39922.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
